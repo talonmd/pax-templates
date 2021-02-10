f0d9e2fa6789e98d0068741921c03146f1794eb1
Talon Durrant <talonmdurrant@gmail.com>
2021-02-10 14:17:51 -0700

    built out page hero layout component

---
+++ components/PageHero.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const PageHero = () => {
+  return (
+    <div class="page-header">
+      <div class="sec-header__hero-image"></div>
+    </div>
+  )
+}
+
+export default PageHero
