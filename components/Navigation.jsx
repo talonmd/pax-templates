94fb37cb0b1c8d6fbd4872bad65f205ba249ebb5
Talon Durrant <talonmdurrant@gmail.com>
2021-06-16 22:16:14 -0400

    using json for dynamic navigation items

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react"
 import Link from "next/link"
 
+import navigationConfig from "../content/settings/navigation.json"
+
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false)
+
+  const { get_started, rules, explore_avarice, digital_tools, community } = navigationConfig
 
   return (
     <nav className={`navigation ${!isOpen ? "closed" : ""}`}>
@@ -20,31 +24,16 @@ const Navigation = () => {
             <a className="navigation__link">Get Started</a>
           </li>
         </Link>
-
         <ul className="navigation__list--nested">
-          <Link href="/get-started/about-avarice">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">About Avarice</a>
-            </li>
-          </Link>
-
-          <Link href="/get-started/quickstart-rules">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Quickstart Rules</a>
-            </li>
-          </Link>
-
-          <Link href="/get-started/resources">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Resources</a>
-            </li>
-          </Link>
-
-          <Link href="/get-started/pricing">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Pricing</a>
-            </li>
-          </Link>
+          {get_started.map((listItem, index) => {
+            return (
+              <Link key={index} href={`/get-started/${listItem.slug}`}>
+                <li className="navigation__item--nested">
+                  <a className="navigation__link--nested">{listItem.page}</a>
+                </li>
+              </Link>
+            )
+          })}
         </ul>
 
         <Link href="/rules">
@@ -54,17 +43,15 @@ const Navigation = () => {
         </Link>
 
         <ul className="navigation__list--nested">
-          <Link href="/rules/player-rules">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Player Rules</a>
-            </li>
-          </Link>
-
-          <Link href="/rules/cc-rules">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">CC Rules</a>
-            </li>
-          </Link>
+          {rules.map((listItem, index) => {
+            return (
+              <Link key={index} href={`/rules/${listItem.slug}`}>
+                <li className="navigation__item--nested">
+                  <a className="navigation__link--nested">{listItem.page}</a>
+                </li>
+              </Link>
+            )
+          })}
         </ul>
 
         <Link href="/explore">
@@ -74,29 +61,15 @@ const Navigation = () => {
         </Link>
 
         <ul className="navigation__list--nested">
-          <Link href="/explore/races">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Races</a>
-            </li>
-          </Link>
-
-          <Link href="/explore/classes">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Classes</a>
-            </li>
-          </Link>
-
-          <Link href="/explore/suronis">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">S.U.R.O.N.I.S.</a>
-            </li>
-          </Link>
-
-          <Link href="/explore/world">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">World</a>
-            </li>
-          </Link>
+          {explore_avarice.map((listItem, index) => {
+            return (
+              <Link key={index} href={`/explore/${listItem.slug}`}>
+                <li className="navigation__item--nested">
+                  <a className="navigation__link--nested">{listItem.page}</a>
+                </li>
+              </Link>
+            )
+          })}
         </ul>
 
         <Link href="/digital-tools">
@@ -106,29 +79,15 @@ const Navigation = () => {
         </Link>
 
         <ul className="navigation__list--nested">
-          <Link href="/digital-tools/avarice-sheets">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Avarice Sheets</a>
-            </li>
-          </Link>
-
-          <Link href="/digital-tools/avarice-database">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Avarice Database</a>
-            </li>
-          </Link>
-
-          <Link href="/digital-tools/pdfs">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">PDFS</a>
-            </li>
-          </Link>
-
-          <Link href="/digital-tools/ebooks">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Ebooks</a>
-            </li>
-          </Link>
+          {digital_tools.map((listItem, index) => {
+            return (
+              <Link key={index} href={`/digital-tools/${listItem.slug}`}>
+                <li className="navigation__item--nested">
+                  <a className="navigation__link--nested">{listItem.page}</a>
+                </li>
+              </Link>
+            )
+          })}
         </ul>
 
         <Link href="/community">
@@ -138,35 +97,15 @@ const Navigation = () => {
         </Link>
 
         <ul className="navigation__list--nested">
-          <Link href="/community/blog">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Blog</a>
-            </li>
-          </Link>
-
-          <Link href="/community/forum">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Forum</a>
-            </li>
-          </Link>
-
-          <Link href="/community/writers-guild">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Writers Guild</a>
-            </li>
-          </Link>
-
-          <Link href="/community/social-media">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Social Media</a>
-            </li>
-          </Link>
-
-          <Link href="/community/support">
-            <li className="navigation__item--nested">
-              <a className="navigation__link--nested">Help &amp; Support</a>
-            </li>
-          </Link>
+          {community.map((listItem, index) => {
+            return (
+              <Link key={index} href={`/community/${listItem.slug}`}>
+                <li className="navigation__item--nested">
+                  <a className="navigation__link--nested">{listItem.page}</a>
+                </li>
+              </Link>
+            )
+          })}
         </ul>
       </ul>
     </nav>
