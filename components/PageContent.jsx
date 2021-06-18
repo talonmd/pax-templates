2c445cb92b59df076253ccdaf5201028d8c95b08
Talon Durrant <talonmdurrant@gmail.com>
2021-06-17 20:49:48 -0400

    class to className

--- components/PageContent.jsx
+++ components/PageContent.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const PageContent = ({ children }) => {
-  return <main class="page-content">{children}</main>
+  return <main className="page-content">{children}</main>
 }
 
 export default PageContent
