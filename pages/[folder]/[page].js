fc2ed76583bff48dfd402d36250503b3dabb573b
Talon Durrant <talonmdurrant@gmail.com>
2021-06-18 03:11:39 -0400

    getting the page dynamic template set up

--- pages/[folder]/[page].js
+++ pages/[folder]/[page].js
@@ -40,30 +40,69 @@ export async function getStaticProps({ ...ctx }) {
 }
 
 export async function getStaticPaths() {
-  // loop through all the files in the pages directory with an IFE
-  const pageSlugs = ((context) => {
-    const keys = context.keys()
+  // import in the navigation config
+  const navigationConfig = await import(`../../content/navigation/navigation.json`)
+  // destructure the data needed
+  const {
+    default: { navigation_order },
+  } = navigationConfig
+  // map that array into an array of folder slugs
+  const folderNames = navigation_order.map((obj) => {
+    return string_to_slug(obj.navigation_group)
+  })
 
-    // generate slugs
-    const data = keys.map((key, index) => {
-      let slug = key.replace(/^.*[\\\/]/, "").slice(0, -3)
+  const folderConfigs = await Promise.all(
+    folderNames.map(async (folderName) => {
+      const fileData = await import(`../../content/navigation/${folderName}.md`)
 
-      // return slug for each file
-      return slug
+      const { data } = matter(fileData.default)
+
+      const pageSlugs = data.pages.map(({ page }) => {
+        return { params: { folder: folderName, page: string_to_slug(page) } }
+      })
+
+      console.log(pageSlugs)
+
+      return pageSlugs
     })
+  )
 
-    // return the array of slugs
-    return data
-  })(require.context("../../content/pages", true, /\.md$/))
+  console.log("FOLDER CONFIGS", folderConfigs)
+
+  let combined = []
+
+  folderConfigs.forEach((folderConfig) => (combined = [...combined, ...folderConfig]))
+
+  console.log(combined)
 
   // set the paths property
-  const paths = pageSlugs.map((slug) => `/${FOLDER}/${slug}`)
+  //const paths = pageSlugs.map((slug) => `/${FOLDER}/${slug}`)
 
-  console.log(paths)
+  //console.log(paths)
 
   // return the required configurations
   return {
-    paths,
+    paths: combined,
     fallback: false,
   }
+}
+
+// utility
+function string_to_slug(str) {
+  str = str.replace(/^\s+|\s+$/g, "") // trim
+  str = str.toLowerCase()
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;"
+  var to = "aaaaeeeeiiiioooouuuunc------"
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i))
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+    .replace(/\s+/g, "-") // collapse whitespace and replace by -
+    .replace(/-+/g, "-") // collapse dashes
+
+  return str
 }
